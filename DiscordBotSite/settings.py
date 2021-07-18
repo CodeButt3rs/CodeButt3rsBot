4be4d6ba421d7586e659b8c7b6bd6bebbc71df23
CodeButt3rs <tekrimon4ever@yandex.ru>
2021-07-18 16:27:18 +0500

    🛠️Uncommented TZ and LANGUEAGE_CODE

--- DiscordBotSite/settings.py
+++ DiscordBotSite/settings.py
@@ -115,15 +115,15 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'Asia/Yekaterinburg'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 # USE_I18N = True
 
 # USE_L10N = True
 
-# USE_TZ = True
+USE_TZ = True
 
 
 # # Static files (CSS, JavaScript, Images)
